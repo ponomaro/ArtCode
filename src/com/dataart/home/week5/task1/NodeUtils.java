94d6d84284691ab5e4d70fcaac9e8a7fc2a70211
ponomaro <olegponomarenko2@gmail.com>
2016-02-09 13:02:14 +0200

    update

--- src/com/dataart/home/week5/task1/NodeUtils.java
+++ src/com/dataart/home/week5/task1/NodeUtils.java
@@ -10,24 +10,27 @@ public class NodeUtils {
     static List<Node> templist = new ArrayList<>();
 
     public static Node addToHead(Node newNode, Node chainHead) {
+        if (chainHead == null) return newNode;
         newNode.next = chainHead;
         return newNode;
     }
 
 
     public static void addToTail(Node newNode, Node chainHead) {
+        if (chainHead == null) return;
         if (chainHead.next != null) addToTail(newNode, chainHead.next);
         if (chainHead.next == null) chainHead.next = newNode;
     }
 
     public static String toString(Node chain) {
+        if (chain == null) return "";
         if (chain.next == null) return ((Integer) chain.value).toString();
         return ((Integer) chain.value).toString() + "; " + toString(chain.next);
     }
 
     public static Node[] toArray(Node chain) {
-        if (chain.next == null) {
-            templist.add(chain);
+        if (chain == null || chain.next == null) {
+            if (chain != null) templist.add(chain);
             Node list2[] = new Node[templist.size()];
             templist.toArray(list2);
             templist.clear();
@@ -40,8 +43,14 @@ public class NodeUtils {
 
 
     public static Node insertByIndex(int index, Node newNode, Node chain) {
+        if (chain == null || newNode == null || index < 0) return chain;
+        if (chain.next == null && index > 0) {
+            System.out.println("Unable to add no such index");
+            return chain;
+        }
 
         if (index > 0) insertByIndex(index - 1, newNode, chain.next);
+
         if (index == 0) {
 
             return addToHead(newNode, chain);
@@ -53,25 +62,40 @@ public class NodeUtils {
     }
 
     public static Node remove(int index, Node chain) {
+        if (chain == null) return null;
         if (index == 0) return chain.next;
-        if (index > 0) remove(index - 1, chain.next);
+        if (index > 0) {
+            if (chain.next != null) {
+                remove(index - 1, chain.next);
+            } else {
+                System.out.println("unable to remove item id that is not in chain");
+                return chain;
+            }
+        }
         if (index == 1) chain.next = chain.next.next;
         return chain;
     }
 
     // use equals method
     public static int indexOf(Object target, Node chain) {
+        if (chain == null) return -1;
         if (chain.equals(target)) return 0;
-        return 1 + indexOf(target, chain.next);
+        if (chain.next == null) return -1;
+        int t = indexOf(target, chain.next);
+        if (t >= 0) return 1 + t;
+        else return -1;
+
     }
 
     // 1,2,3,4,5 - 5,4,3,2,1
     // create new
     // use existed, one loop
     // advanced *
+
     static Node revert;
 
     public static Node reverse(Node chain) {
+        if (chain==null||chain.next==null) return chain;
         reverse(chain, chain);
         chain.next = null;
         return revert;
--- src/com/dataart/home/week5/task1/TestNode.java
+++ src/com/dataart/home/week5/task1/TestNode.java
@@ -12,30 +12,55 @@ public class TestNode {
     public static void main(String[] args) {
         Node head = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5, null)))));
 
-
+        /*
+                        positive testing
+         */
         //test node add to head
         Node node1 = new Node(0, null);
-        System.out.println("test add to head: "+ NodeUtils.toString(addToHead(node1, head)).equals("0; 1; 2; 3; 4; 5"));
+        System.out.println("test add to head: " + NodeUtils.toString(addToHead(node1, head)).equals("0; 1; 2; 3; 4; 5"));
         // test nod add to tail
         Node node6 = new Node(6, null);
         addToTail(node6, head);
-        System.out.println("test add to tail: "+NodeUtils.toString(head).equals("1; 2; 3; 4; 5; 6"));
+        System.out.println("test add to tail: " + NodeUtils.toString(head).equals("1; 2; 3; 4; 5; 6"));
         //array
         Node ar1[] = toArray(head);
         Arrays.toString(ar1);
-        System.out.println("test to array: "+(ar1.length==6));
+        System.out.println("test to array: " + (ar1.length == 6));
         // insert by index
         Node node7 = new Node(7, null);
-        System.out.println("test insert by index: "+NodeUtils.toString(insertByIndex(3, node7, head)).equals("1; 2; 3; 7; 4; 5; 6"));
+        System.out.println("test insert by index: " + NodeUtils.toString(insertByIndex(3, node7, head)).equals("1; 2; 3; 7; 4; 5; 6"));
         // remove
-        System.out.println("test remove: "+NodeUtils.toString(remove(3, head)).equals("1; 2; 3; 4; 5; 6"));
+        System.out.println("test remove: " + NodeUtils.toString(remove(3, head)).equals("1; 2; 3; 4; 5; 6"));
         // equels
         Node node8 = new Node(7, node7);
-        System.out.println("test equels: "+node7.equals(node8));
+        System.out.println("test equels: " + node7.equals(node8));
         // getIndexOf
-        System.out.println("test getIndexOf: "+(indexOf(node6, head)==5));
+        System.out.println("test getIndexOf: " + (indexOf(node6, head) == 5));
         // reverse
-        System.out.println("test reverse: "+NodeUtils.toString(reverse(head)).equals("6; 5; 4; 3; 2; 1"));
+        System.out.println("test reverse: " + NodeUtils.toString(head = reverse(head)).equals("6; 5; 4; 3; 2; 1"));
+
+        /*
+                        negative testing
+         */
+        System.out.println("negative testing _____________________________________________________________________");
+        //test node add to head
+        System.out.println("test add to head: " + (addToHead(node1, null)==node1));
+        //array
+        Node ar2[] = toArray(null);
+        System.out.println("test to array: " + (ar2.length == 0));
+        // insert by index
+        Node node11 = new Node(11, null);
+
+        System.out.println("test insert by index: " + NodeUtils.toString(head=insertByIndex(11, node11, head)).equals("6; 5; 4; 3; 2; 1"));
+        // remove
+        System.out.println("test remove: " + NodeUtils.toString(remove(15, head)).equals("6; 5; 4; 3; 2; 1"));
+
+        // getIndexOf
+        System.out.println("test getIndexOf: " + (indexOf(node11, head) == -1));
+        // reverse
+        System.out.println("test reverse1: "+(reverse(null)==null));
+        System.out.println("test reverse2: " + NodeUtils.toString(head = reverse(head)).equals("1; 2; 3; 4; 5; 6"));
+
     }
 
 
